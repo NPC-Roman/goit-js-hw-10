f259bc5cd79f8b43eccb1441e13e1bf9b5e98858
NPC-Roman <ispacebobo@gmail.com>
2023-09-02 15:38:28 +0300

    Change 6

--- src/index.js
+++ src/index.js
@@ -18,16 +18,7 @@ const fetchBreeds = () => {
     return response.json();
   });
 };
-/*
-const fetchCatByBreed = breedId => {
-  return fetch(`${url2}/${breedId}?api_key=${KEY}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-};
-*/
+
 const renderBreedDesc = breed => {
   const markupPicture = `<img class="cat-picture" src="${breed.url}" alt="${breed.id}">`;
   const markupDescript = `<h2 class="cat-info-desc-title">${breed.breeds[0].name}</h2>
@@ -111,7 +102,6 @@ const fetchAndRenderBreeds = () => {
 const KEY =
   'live_SuxLaeL5ZwQ8jCf9HLGaCILAmryULP7zAomPmG0UGGJU0M3xHqLTfFbcVBurOAq8';
 const url1 = 'https://api.thecatapi.com/v1/breeds';
-/*const url2 = 'https://api.thecatapi.com/v1/images';*/
 
 elements.breedSelect.addEventListener('change', onChangeSelect);
 
